220c8157387863c8f409842b43b051f334da5fc2
sbaijal <sbaijal99@gmail.com>
2026-05-14 18:48:24 +0530

    fetching S&P500 data

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ ticker = '^GSPC'
 
 OBSERVATION_START_DATE = '2000-01-01'
 OBSERVATION_END_DATE = '2025-12-31'
+
+TRAIN_END   = '2012-12-31'   # End of burn-in window
+EVAL_START  = '2013-01-01'   # Start of rolling evaluation
+EVAL_END    = '2025-12-31'   # End of rolling evaluation
